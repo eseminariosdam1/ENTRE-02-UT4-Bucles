725294c332b7b1dad854f45cb0f0ad97b4897748
Eneko Seminario <eseminariosdam1@gmail.com>
2020-11-10 18:17:51 +0100

    Clase Utilidades terminada

--- Utilidades.java
+++ Utilidades.java
@@ -17,8 +17,16 @@ public class Utilidades
      * (usa bucles while)
      */
     public static boolean estaEnOctal(int n) {
+        boolean esOctal = false;
+        while(n != 0){
+          if(n % 10 >= 0 && n % 10 <= 7 ){
+            esOctal = true;
+          }else{
+            return esOctal;
+          }
+        }
          
-        return true;
+        return esOctal;
 
     }
 
@@ -29,12 +37,12 @@ public class Utilidades
      * (usa bucles while)
      */
     public static int contarCifras(int n) {
-        int cifras = 0;
+        int contadorCifras = 0;
         while (n != 0){
             n = n / 10;
-            cifras++;
+            contadorCifras++;
         } 
-        return cifras;
+        return contadorCifras;
 
     }
 
---
+++ __SHELL5.java
@@ -0,0 +1,11 @@
+
+public class __SHELL5 extends bluej.runtime.Shell {
+public static java.lang.Object run() throws Throwable {
+
+int __bluej_param0 = 54;
+try {
+return makeObj(Utilidades.estaEnOctal(__bluej_param0)
+);}
+finally {
+}
+}}
